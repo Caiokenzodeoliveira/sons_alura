2c7cb7e9e0d451ceaba005cc2e12a4261240fc05
caio <125563854+Caiokenzodeoliveira@users.noreply.github.com>
2023-10-19 20:26:23 -0300

    Add files via upload
    aa

---
+++ main.js
@@ -0,0 +1,68 @@
+
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+
+document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+
+
+
+document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+
+
+
+document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+
+
+
+document.querySelector('#som_tecla_Splash').play();
+}
+
+document.querySelector('.tecla_Splash').onclick = tocaSomPom;
+
+
+
+
+document.querySelector('#som_tecla_Toim').play();
+}
+
+document.querySelector('.tecla_Toim').onclick = tocaSomPom;
+
+
+
+
+document.querySelector('#som_tecla_Psh').play();
+}
+
+document.querySelector('.tecla_Psh').onclick = tocaSomPom;    
+
+
+
+
+document.querySelector('#som_tecla_Tic').play();
+}
+
+document.querySelector('.tecla_Tic').onclick = tocaSomPom;
+
+
+
+
+document.querySelector('#som_tecla_Tom').play();
+}
+
+document.querySelector('.tecla_Tom').onclick = tocaSomPom;
